9410e8cf4117a77df7a1b5e376bd894d63518f32
akp8008 <ASWATHI_K_PURAKKAL@homedepot.com>
2018-11-01 15:17:45 +0530

    "ngFor ,ngStyle and ngClass functionality"

--- my-dream-app/src/app/server/server.component.ts
+++ my-dream-app/src/app/server/server.component.ts
@@ -3,9 +3,23 @@ import { Component } from "@angular/core";
 @Component({
 selector: 'app-server',
 templateUrl :'./server.component.html',
-styleUrls: ['./server.component.css']
+styles: [`
+.online{
+    color:white;
+}
+`]
 })
 
 export class ServerComponent {
-      
-}
+    serverName = "This is the server component";
+    serverContent =['Test','Test1'];
+    serverStatus = false;
+ 
+    onServerCreated(){
+        this.serverStatus = !this.serverStatus;
+        this.serverContent.push(this.serverName);
+    }
+    getColor(index){
+       return index > 4 ?'blue' : 'none'
+    }
+  }
